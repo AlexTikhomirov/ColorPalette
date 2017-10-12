5adacb96f80f048ae752446ba20ed3f75f5fb641
Alex Tikhomirov <alexandrtikhomirov@gmail.com>
2017-10-13 00:54:42 +0300

    1 relise

--- ColorPaletteTest/ColorPaletteTest/SHTColorViewController.swift
+++ ColorPaletteTest/ColorPaletteTest/SHTColorViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class SHTColorViewController: UIViewController {
+class SHTColorViewController: UIViewController, colorDelegate {
     
-    public var colorState: UIColor = .gray
+    public var colorState: UIColor = .black
     public var colorNew: UIColor = .gray
-
+    //MARK: - Outlets
     @IBOutlet weak var sliderR: UISlider!
     @IBOutlet weak var sliderG: UISlider!
     @IBOutlet weak var sliderB: UISlider!
@@ -28,24 +28,24 @@ class SHTColorViewController: UIViewController {
     @IBOutlet weak var valueLabelA: UILabel!
     @IBOutlet weak var colorStateView: UIView!
     @IBOutlet weak var colorNewView: UIView!
+    @IBOutlet weak var colorPicker: ColorPicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(sliderView.frame)
         sliderTransform(sender: sliderR, index: 0)
         sliderTransform(sender: sliderG, index: 1)
         sliderTransform(sender: sliderB, index: 2)
         sliderTransform(sender: sliderA, index: 3)
+        colorPicker.delegate = self;
         colorNew = colorState
         drawColor()
-        setSliderPosition((R:5, G:6, B:7, A:8))
+        setSliderPosition(convertColor(colorState))
     }
-    
+    //MARK: - Slider Transform
     private func sliderTransform( sender: UISlider, index: Int) {
         if sender.isKind(of: UISlider.self) {
             sender.transform = CGAffineTransform(rotationAngle: CGFloat( .pi * -0.5))
             sender.frame = CGRect(x: sliderView.frame.width - 145.0 + CGFloat(index) * 35.0, y: 30.0, width: 30.0, height: sliderView.frame.height - 60)
-            print(sender.frame)
         }
         switch index {
         case 0:
@@ -63,6 +63,13 @@ class SHTColorViewController: UIViewController {
         }
     }
     
+    private func setSliderPosition(_ color:(r:Int, g:Int, b:Int, a:Int)) {
+        sliderR.value = Float(color.r)
+        sliderG.value = Float(color.g)
+        sliderB.value = Float(color.b)
+        sliderA.value = Float(color.a)
+    }
+    //MARK: - Action
     @IBAction func sliderAction(_ sender: Any) {
         if let slider = sender as? UISlider {
             switch slider.tag {
@@ -75,29 +82,51 @@ class SHTColorViewController: UIViewController {
             default :
                 valueLabelA.text = String(Int(slider.value))
             }
+            colorNew = colorSliderToUIColor()
+            drawColor()
         }
     }
     
+    @IBAction func doneAction(_ sender: Any) {
+        colorState = colorNew
+        if let vc = navigationController?.viewControllers[0] as? ViewController {
+                vc.colorView.backgroundColor = colorState
+        }
+        navigationController?.popViewController(animated: true)
+    }
+    //MARK: - Color funtion
     private func drawColor() {
         colorStateView.backgroundColor = colorState
         colorNewView.backgroundColor = colorNew
     }
     
-    private func setSliderPosition(_ color:(R:Int, G:Int, B:Int, A:Int)) {
-        sliderR.value = Float(color.R)
-        sliderG.value = Float(color.G)
-        sliderB.value = Float(color.B)
-        sliderA.value = Float(color.A)
+    private func convertColor(_ color: UIColor) -> (r:Int, g:Int, b:Int, a:Int) {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        if color.getRed(&r, green: &g, blue: &b, alpha: &a){
+            return (r: Int(r * 255), g: Int(g * 255), b: Int(b * 255), a: Int(a * 255))
+        }
+        return (r: 255, g: 0, b: 255, a: 255)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func colorSliderToUIColor() -> UIColor {
+        let r = CGFloat(sliderR.value / 255)
+        let g = CGFloat(sliderG.value / 255)
+        let b = CGFloat(sliderB.value / 255)
+        let a = CGFloat(sliderA.value / 255)
+        let color = UIColor(red: r, green: g, blue: b, alpha: a)
+        return color
     }
-    */
-
+    
+    func pickedColor(color: UIColor) {
+        setSliderPosition(convertColor(color))
+        valueLabelR.text = String(Int(sliderR.value))
+        valueLabelG.text = String(Int(sliderG.value))
+        valueLabelB.text = String(Int(sliderB.value))
+        valueLabelA.text = String(Int(sliderA.value))
+        colorNew = color
+        drawColor()
+    }
 }
